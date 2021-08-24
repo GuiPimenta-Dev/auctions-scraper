3b6f45d64a38ea75c7668ffa7283373ed956a9bb
GUILHERME PIMENTA <guilherme.pimentahays@modec.com>
2021-08-24 15:41:52 -0300

    sato leiloes

---
+++ auctions/spiders/satoleiloes.py
@@ -0,0 +1,47 @@
+import scrapy
+from scrapy import FormRequest
+
+from ..items import AuctionsItem
+from ..utils.parser import Parser
+
+
+class SatoleiloesSpider(scrapy.Spider):
+    name = 'satoleiloes'
+    parser = Parser()
+
+    start_urls = ['https://www.satoleiloes.com.br']
+
+    def parse(self, response):
+        data = {
+            '_method': 'POST',
+            'data[onde]': 'descricao',
+            'data[Bem][termo]': self.city
+        }
+
+        url = 'https://www.satoleiloes.com.br/externo/bens/pesquisaAvancada'
+
+        yield FormRequest(url=url, formdata=data, callback=self.parse_response)
+
+    def parse_response(self, response):
+        item = AuctionsItem()
+        divs = response.xpath('//div[@class="c-lote"]').extract()
+        for div in divs:
+            description = self.parser.get_single_value_from_string(raw_string=div,
+                                                                   xpath='//div[@class="c-lote-descricao"]//a/text()')
+            under_description = description.lower()
+            if 'apartamento' in under_description or 'casa' in under_description or 'comercial' in under_description or 'residencial' in under_description or 'rural' in under_description or 'terreno' in under_description:
+                item['site'] = 'Sato Leilões'
+
+                item['price'] = self.parser.get_single_value_from_string(raw_string=div, xpath='//big/text()')
+
+                item['url'] = 'https://www.satoleiloes.com.br' + self.parser.get_single_value_from_string(
+                    raw_string=div, xpath='//a/@href')
+
+                item['description'] = description
+
+                yield item
+
+        next = response.xpath('//a[@rel="next"]/@href').get()
+        if next:
+            yield response.follow(url=next, callback=self.parse_response)
+
