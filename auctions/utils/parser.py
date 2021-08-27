5a1e1d3eb3a75ee1ec72d44c9d4574b1b7934017
GUILHERME PIMENTA <guilherme.pimentahays@modec.com>
2021-08-27 11:35:17 -0300

    Adding Galpão to parser

--- auctions/utils/parser.py
+++ auctions/utils/parser.py
@@ -123,10 +123,6 @@ class Parser:
             return 'Barracão'
         elif 'residencial' in under_description:
             return 'Residencial'
-        elif 'sala comercial' in under_description:
-            return 'Sala comercial'
-        elif 'prédio comercial' in under_description or ' predio comercial' in under_description:
-            return 'Prédio comercial'
         elif 'comercial' in under_description:
             return 'Comercial'
         elif 'casa' in under_description:
