282e8fd6ec58918ad76a2394356bd95a120c644b
GUILHERME PIMENTA <guilherme.pimentahays@modec.com>
2021-09-19 17:58:15 -0300

    flask api

--- api.py
+++ api.py
@@ -1,17 +1,18 @@
-import os
-from flask_runner import Runner
 from flask import Flask, request
 from flask_cors import CORS
+
+from flask_runner import Runner
 
 app = Flask(__name__)
 CORS(app)
 runner = Runner()
 
+
 @app.route("/")
 def run_robots():
     state_city = request.args.get('state_city', '')
-    data = runner.run_robots(state_city)
-    return data
+    return runner.run_robots(state_city)
+
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
