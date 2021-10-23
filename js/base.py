9fb9d6a7dce106ea39b8ac2d4610fc973c30f86e
GUILHERME PIMENTA <guilherme.pimentahays@modec.com>
2021-10-23 20:02:10 -0300

    caixa refactored

--- js/base.py
+++ js/base.py
@@ -4,17 +4,18 @@ from js_parser import Parser
 from scrapy import Selector
 
 
-
 class BaseRequests(Parser):
     @staticmethod
-    def parse_json_response(url: str, query_params: dict = None, headers: dict = None):
-        response = requests.request("GET", url, params=query_params, headers=headers)
+    def parse_json_response(url: str, query_params: dict = None, headers: dict = None,
+                            payload: dict = None):
+        response = requests.request('GET', url, params=query_params, headers=headers, payload=payload)
 
         return json.loads(response.text)
 
     @staticmethod
-    def parse_html_response(url: str, query_params: dict = None, headers: dict = None):
-        response = requests.request("GET", url, params=query_params, headers=headers)
+    def parse_html_response(url: str, query_params: dict = None, headers: dict = None,
+                            payload: dict = None):
+        response = requests.request('GET', url, params=query_params, headers=headers, payload=payload)
         response = Selector(text=response.text)
 
         return response
